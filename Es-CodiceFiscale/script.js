b53d18a2f011144c879c991adf3b74b899de44a6
Lorenzo De Angelis <125358763+NotLor@users.noreply.github.com>
2023-09-23 09:52:21 +0000

    z

--- Es-CodiceFiscale/script.js
+++ Es-CodiceFiscale/script.js
@@ -1,21 +1,22 @@
 const fileUrl = "comuni.xlsx";
 var vocali = "AEIOU";
 var consonanti = "BCDFGHJKLMNPQRSTVWXYZ";
-let codiceFiscale = "";
+
 
 var cognome = document.getElementById("cognome");
 var nome = document.getElementById("nome");
 var sesso = document.getElementById("sesso");
 var luogo_nascita = document.getElementById("luogo_nascita");
-var provincia = document.getElementById("provincia");
 var giorno = document.getElementById("giorno");
 var mese = document.getElementById("mese");
 var anno = document.getElementById("anno");
 var calcola = document.getElementById("calcola");
 
 calcola.onclick = async function(){
+    let codiceFiscale = "";
+
     // COGNOME*********************************************************************************************************************
-    // primi 3 caratteri
+
     cognome.value = cognome.value.replace(/\s/g, '');
     //  Trasformo il cognome in maiuscolo
     let cognomeMaiuscolo = cognome.value.toUpperCase();
@@ -204,11 +205,357 @@ calcola.onclick = async function(){
     if (err) return "errore nell'inserimento del luogo di nascità";
      
     // CARATTERE DI CONTROLLO*********************************************************************************************************************
-    // Calcolo il carattere di controllo
     
+    /*
+    1. ciascuno degli anzidetti quindici caratteri, a seconda che occupi posizione di ordine pari o posizione di ordine dispari, viene convertito in un valore numerico, in base alle tabelle di corrispondenza successivamente riportate.
+
+    Per i sette caratteri con posizione di ordine pari:
+
+    Tabella C – conversione dei caratteri con posizione di ordine pari
+    A o 0 = 0	F o 5 = 5	K = 10	P = 15	U = 20
+    B o 1 = 1	G o 6 = 6	L = 11	Q = 16	V = 21
+    C o 2 = 2	H o 7 = 7	M = 12	R = 17	W = 22
+    D o 3 = 3	I o 8 = 8	N = 13	S = 18	X = 23
+    E o 4 = 4	J o 9 = 9	O = 14	T = 19	Y = 24
+    Z = 25
+    Per gli otto caratteri con posizione di ordine dispari:
+
+    Tabella D– conversione dei caratteri con posizione di ordine dispari
+    A o 0 = 1	F o 5 = 13	K = 2	P = 3	U = 16
+    B o 1 = 0	G o 6 = 15	L = 4	Q = 6	V = 10
+    C o 2 = 5	H o 7 = 17	M = 18	R = 8	W = 22
+    D o 3 = 7	I o 8 = 19	N = 20	S = 12	X = 25
+    E o 4 = 9	J o 9 = 21	O = 11	T = 14	Y = 24
+    Z = 23
+    */
+    
+    // Calcolo la somma dei valori numerici dei caratteri
+    let somma = 0;
+    for (let i = 0; i < codiceFiscale.length; i++){
+        if(i%2 == 0){//tabella dispari
+            switch(codiceFiscale[i]){
+                case "A":
+                    somma += 1;
+                    break;
+                case "0":
+                    somma += 1;
+                    break;
+                case "B":
+                    somma += 0;
+                    break;
+                case "1":
+                    somma += 0;
+                    break;
+                case "C":
+                    somma += 5;
+                    break;
+                case "2":
+                    somma += 5;
+                    break;
+                case "D":
+                    somma += 7;
+                    break;
+                case "3":
+                    somma += 7;
+                    break;
+                case "E":
+                    somma += 9;
+                    break;
+                case "4":
+                    somma += 9;
+                    break;
+                case "F":
+                    somma += 13;
+                    break;
+                case "5":
+                    somma += 13;
+                    break;
+                case "G":
+                    somma += 15;
+                    break;
+                case "6":
+                    somma += 15;
+                    break;
+                case "H":
+                    somma += 17;
+                    break;
+                case "7":
+                    somma += 17;
+                    break;
+                case "I":
+                    somma += 19;
+                    break;
+                case "8":
+                    somma += 19;
+                    break;
+                case "J":
+                    somma += 21;
+                    break;
+                case "9":
+                    somma += 21;
+                    break;
+                case "K":
+                    somma += 2;
+                    break;
+                case "L":
+                    somma += 4;
+                    break;
+                case "M":
+                    somma += 18;
+                    break;
+                case "N":
+                    somma += 20;
+                    break;
+                case "O":
+                    somma += 11;
+                    break;
+                case "P":
+                    somma += 3;
+                    break;
+                case "Q":
+                    somma += 6;
+                    break;
+                case "R":
+                    somma += 8;
+                    break;
+                case "S":
+                    somma += 12;
+                    break;
+                case "T":
+                    somma += 14;
+                    break;
+                case "U":
+                    somma += 16;
+                    break;
+                case "V":
+                    somma += 10;
+                    break;
+                case "W":
+                    somma += 22;
+                    break;
+                case "X":
+                    somma += 25;
+                    break;
+                case "Y":
+                    somma += 24;
+                    break;
+                case "Z":
+                    somma += 23;
+                    break;
+            }
+        }else{//tabella pari
+            switch(codiceFiscale[i]){
+                case "A":
+                    somma += 0;
+                    break;
+                case "0":
+                    somma += 0;
+                    break;
+                case "B":
+                    somma += 1;
+                    break;
+                case "1":
+                    somma += 1;
+                    break;
+                case "C":
+                    somma += 2;
+                    break;
+                case "2":
+                    somma += 2;
+                    break;
+                case "D":
+                    somma += 3;
+                    break;
+                case "3":
+                    somma += 3;
+                    break;
+                case "E":
+                    somma += 4;
+                    break;
+                case "4":
+                    somma += 4;
+                    break;
+                case "F":
+                    somma += 5;
+                    break;
+                case "5":
+                    somma += 5;
+                    break;
+                case "G":
+                    somma += 6;
+                    break;
+                case "6":
+                    somma += 6;
+                    break;
+                case "H":
+                    somma += 7;
+                    break;
+                case "7":
+                    somma += 7;
+                    break;
+                case "I":
+                    somma += 8;
+                    break;
+                case "8":
+                    somma += 8;
+                    break;
+                case "J":
+                    somma += 9;
+                    break;
+                case "9":
+                    somma += 9;
+                    break;
+                case "K":
+                    somma += 10;
+                    break;
+                case "L":
+                    somma += 11;
+                    break;
+                case "M":
+                    somma += 12;
+                    break;
+                case "N":
+                    somma += 13;
+                    break;
+                case "O":
+                    somma += 14;
+                    break;
+                case "P":
+                    somma += 15;
+                    break;
+                case "Q":
+                    somma += 16;
+                    break;
+                case "R":
+                    somma += 17;
+                    break;
+                case "S":
+                    somma += 18;
+                    break;
+                case "T":
+                    somma += 19;
+                    break;
+                case "U":
+                    somma += 20;
+                    break;
+                case "V":
+                    somma += 21;
+                    break;
+                case "W":
+                    somma += 22;
+                    break;
+                case "X":
+                    somma += 23;
+                    break;
+                case "Y":
+                    somma += 24;
+                    break;
+                case "Z":
+                    somma += 25;
+                    break;
+            }
+        }
+    }
+    
+    /*
+    2. I valori numerici così determinati vengono addizionati e la somma si divide per il numero 26. Il carattere di controllo si ottiene convertendo il resto di tale divisione nel carattere alfabetico ad esso corrispondente nella sotto indicata tabella:
+
+    Tabella E – determinazione del "check-digit"
+    0 = A	5 = F	10 = K	15 = P	20 = U
+    1 = B	6 = G	11 = L	16 = Q	21 = V
+    2 = C	7 = H	12 = M	17 = R	22 = W
+    3 = D	8 = I	13 = N	18 = S	23 = X
+    4 = E	9 = J	14 = O	19 = T	24 = Y
+    25 = Z
+    */
+    // Calcolo il carattere di controllo
+    let resto = somma % 26;
+
+    switch(resto){
+        case 0:
+            codiceFiscale += "A";
+            break;
+        case 1:
+            codiceFiscale += "B";
+            break;
+        case 2:
+            codiceFiscale += "C";
+            break;
+        case 3:
+            codiceFiscale += "D";
+            break;
+        case 4:
+            codiceFiscale += "E";
+            break;
+        case 5:
+            codiceFiscale += "F";
+            break;
+        case 6:
+            codiceFiscale += "G";
+            break;
+        case 7:
+            codiceFiscale += "H";
+            break;
+        case 8:
+            codiceFiscale += "I";
+            break;
+        case 9:
+            codiceFiscale += "J";
+            break;
+        case 10:
+            codiceFiscale += "K";
+            break;
+        case 11:
+            codiceFiscale += "L";
+            break;
+        case 12:
+            codiceFiscale += "M";
+            break;
+        case 13:
+            codiceFiscale += "N";
+            break;
+        case 14:
+            codiceFiscale += "O";
+            break;
+        case 15:
+            codiceFiscale += "P";
+            break;
+        case 16:
+            codiceFiscale += "Q";
+            break;
+        case 17:
+            codiceFiscale += "R";
+            break;
+        case 18:
+            codiceFiscale += "S";
+            break;
+        case 19:
+            codiceFiscale += "T";
+            break;
+        case 20:
+            codiceFiscale += "U";
+            break;
+        case 21:
+            codiceFiscale += "V";
+            break;
+        case 22:
+            codiceFiscale += "W";
+            break;
+        case 23:
+            codiceFiscale += "X";
+            break;
+        case 24:
+            codiceFiscale += "Y";
+            break;
+        case 25:
+            codiceFiscale += "Z";
+            break;
+    }
+
+
     console.log(codiceFiscale);
-        
+    
+    //stampo il codice fiscale finale
+    document.getElementById("risultato").innerHTML = codiceFiscale;
+    
 }
-
-
-
